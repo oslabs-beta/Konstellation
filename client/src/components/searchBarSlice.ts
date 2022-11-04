123fe148bdec135298047f97fdb4873d85f2efcd
Jonathan Le <lejon1220@gmail.com>
2022-11-04 12:32:51 -0700

    refactored searchbarslice

---
+++ client/src/components/searchBarSlice.ts
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState, AppThunk } from "../lib/store";
+import { config } from '../constants/config'
+
+export type NamespaceData = NamespaceElement[];
+
+
+export interface NamespaceElement {
+	name: string
+}
+
+export interface Search {
+  type: 'cluster' | 'node'
+	namespace: NamespaceData | undefined
+}
+
+const initialState:Search = {
+	type: 'cluster',
+	namespace: undefined
+}
+
+const searchReducer = createSlice({
+	name: 'searchBar',
+	initialState: initialState,
+	reducers: {
+		updateData:  (state, action: PayloadAction<NamespaceData>) => {
+      state.namespace = action.payload;
+  	}
+  }
+})
+
+
+
+
+
+export default searchReducer.reducer;
