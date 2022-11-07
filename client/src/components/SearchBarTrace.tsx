518af08baab229a237e7cc902ba45486501bbea2
Jonathan Le <lejon1220@gmail.com>
2022-11-07 09:51:50 -0800

    building out features of the search bar  trace view

--- client/src/components/SearchBarTrace.tsx
+++ client/src/components/SearchBarTrace.tsx
@@ -1,21 +1,27 @@
 import React, { JSXElementConstructor, useEffect } from 'react';
-import { selectSourceMapData } from './sourceMapSlice';
+import { changeView, selectSourceMapData, updateData } from './sourceMapSlice';
 import { useAppSelector, useAppDispatch } from '../lib/hooks';
 import { trace } from 'console';
 
 const SearchBarTrace = (): JSX.Element => {
 	
-  
-  const submitTrace = ():any => {
+	const dispatch = useAppDispatch();
+  const submitTrace = (traceID?:string | undefined):any => {
     //updates the view data to reflect the new trace
-		
+		console.log(traceID)
+		dispatch(updateData({type: 1, data:traceID}))
 		return
 		
 	}
 
   const returnToCLusterView = ():any => {
     //returns the view to the cluster view
+    dispatch( changeView({type: 0}))
 		return
+	}
+
+	const handleClick = ():any => {
+		//closes this trace details view?
 	}
 
 	const traceID = useAppSelector(selectSourceMapData);
@@ -36,15 +42,56 @@ const SearchBarTrace = (): JSX.Element => {
 					  name='traceID'
 					  placeholder='Enter TraceID, node, pod, or service name'  
 					  />
-				  <button id="submitButton" onClick={()=>submitTrace()}> submit</button>
+				  <button id="submitButton" onClick={()=>{
+						const input = document.getElementById('searchBarInput') as HTMLInputElement;
+						submitTrace(input.value)}}> submit</button>
 					<button id="returnButton" onClick={()=>returnToCLusterView()}>Cluster View</button>
 			  </div>
 			</div>
 			<div id="traceSearchBarTraceDetails">
-				<div>
+				<div id='traceInfoBarContainer'>
+				<div id='traceInfoBar'>
+					  <div id="traceMapSearchBar">
+					  Trace Map
+					  </div>
+					  <div className="searchTextPrefix">
+						Trace ID:
+					  </div>
+					  <div className='searchData'>
 					{traceID}
+					  </div>
+					  <div className="searchTextPrefix">
+						Trace Start:
+					  </div>
+				  	<div className='searchData'>
+						10/12/2022 10:21 AM
+					  </div>
+					  <div className="searchTextPrefix">
+						Total Duration:
+					  </div>
+					  <div className='searchData'>
+						4.03 ms
+					  </div>
+					  <div className="searchTextPrefix">
+						Services:
+					  </div>
+					  <div className='searchData'>
+						3
+					  </div>
+					  <div className="searchTextPrefix">
+						Total Spans:
+					  </div>
+					  <div className='searchData'>
+						6
+					  </div>
+					  </div>
+					<div>
+					  <span className="close" onClick={handleClick}>&times;</span>
+					</div>
 				</div>
-				specific trace details at a glance here
+			</div>
+			<div className='barGraph'>
+				Bar Graph Here
 			</div>
 		</div>
 	)
